a5386d82460dd246daa55e3aeaa346803461264f
Nisha <rainishani400@gmail.com>
2025-06-03 17:05:27 +0545

    Add files via upload

---
+++ day3/TodoList.jsx
@@ -0,0 +1,69 @@
+
+import { useState } from "react";
+
+
+
+
+import { v4 as uuidv4 } from 'uuid';
+
+
+export default function TodoList(){
+    let [todos,setTodos]= useState([ {task : "sample task", id: uuidv4()}]  );
+    let [newTodo,setNewTodo]= useState("");
+    
+
+    let  addNewTask=()=>{
+setTodos((prevTodos)=>{
+    return[...todos, {task:newTodo, id:uuidv4()}]
+
+});
+
+setNewTodo("");
+    };
+
+    let updateTodoValue =(event)=>{
+        setNewTodo(event.target.value);
+    };
+let deleteTodo=(id)=>{
+   setTodos((prevTodos)=>todos.filter((prevTodos) => prevTodos.id != id));
+};
+let upperCaseAll = ()=>{
+
+    setTodos(todos.map((todo)=>{
+        return{
+            ...todo,
+            task: todo.task.toLocaleUpperCase(),
+        };
+    })
+);
+};
+
+
+
+    return(
+        <div>
+            <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
+            <br></br>
+ <button onClick={addNewTask}>Add Task</button>
+ <br></br><br></br>
+ <br></br>
+ <br></br>
+ 
+ <hr/>
+ 
+        <h4> Todo List</h4>
+        <ul>
+            {todos.map((todo)=>(
+                <li key={todo.id}>
+                   <span> {todo.task}</span>
+                   &nbsp;  &nbsp;  &nbsp;   &nbsp;
+                   <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+
+                   </li>
+            ))}
+        </ul>
+        <br></br>
+        <button onClick={upperCaseAll}>UpperCase All</button>
+        </div>
+    );
+}
