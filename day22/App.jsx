05883441440199584891d0f15b7c2fce852ed195
Nisha <rainishani400@gmail.com>
2025-06-22 20:19:14 +0545

    Add files via upload

---
+++ day22/App.jsx
@@ -0,0 +1,14 @@
+//import Alert from '@mui/material/Alert';
+import SearchBox from './SearchBox';
+import InfoBox from './infoBox';
+function App() {
+
+  return (
+    <>
+      <SearchBox/>
+      <InfoBox/>
+    </>
+  )
+}
+
+export default App
---
+++ day22/InfoBox.jsx
@@ -0,0 +1,45 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function InfoBox() {
+    const INIT_URL = "https://images.unsplash.com/photo-1513496497-82958929944d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    let info = {
+        city:"nepal",
+        feelLike: 24.84,
+        temp: 25.05,
+        tempMin: 25.05,
+        tempMax: 25.05,
+        humidity: 47,
+        weather: "haze",
+
+    };
+
+    return (
+        <div className="InfoBox">
+            <h1>WeatherInfo-{info.weather}</h1>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={INIT_URL}
+                    title="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {info.city}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                       
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </div>
+    );
+}
